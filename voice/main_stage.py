bf52a8c4bac54d560b6d386696e69f1d4e1b06d0
brianhouse <brian.house@gmail.com>
2013-02-08 17:55:16 -0500

    fixed log so it doesnt compete with housepy logs

--- voice/main_stage.py
+++ voice/main_stage.py
@@ -1,9 +1,9 @@
-from . import Voice
+from . import Voice, synth
 
 class MainStage(Voice):
 
     def __init__(self, channel=1, cycles=1):  
         Voice.__init__(self, channel, cycles)
 
-    # def play(self, pitch, velocity):
-    #     synth.send('/braid/note', self.channel, pitch, velocity)
+    def play(self, pitch, velocity):
+        synth.send('/braid/note', self.channel, pitch, int(velocity * 127))
