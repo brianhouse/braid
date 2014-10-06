f951fdde30c576da34c4bf3996eaa19157805785
brianhouse <brian.house@gmail.com>
2014-10-06 17:30:16 -0400

    modified pattern to support probabilities functions

--- braid/pattern.py
+++ braid/pattern.py
@@ -1,4 +1,4 @@
-import braid.tween, braid.attribute
+import braid.tween, braid.attribute, collections
 from braid import controller
 from random import random
 
@@ -31,12 +31,19 @@ class Pattern(list):
     def _pick(self, step):
         """Choose between options for a given step"""
         assert len(step) == 2 or len(step) == 3
+        if len(step) == 3:
+            prob = step[2]() if isinstance(step[2], collections.Callable) else step[2]
+            step = step[0], step[1], prob
         if len(step) == 2:
-            if type(step[1]) == float: # (1, 0.5) is a 50% chance of playing a 1, otherwise 0
-                step = step[0], [0, 0], step[1] ## it's a 0, 0 because 0 patterns dont progress, and this could be the root level: is this a bug?
+            if isinstance(step[1], collections.Callable):
+                prob = step[1]()
+                step = step[0], [0, 0], prob
+            elif type(step[1]) == float : # (1, 0.5) is a 50% chance of playing a 1, otherwise 0
+                prob = step[1]
+                step = step[0], [0, 0], prob                
             else:
                 step = step[0], step[1], 0.5 # (1, 2) is a 50% chance of playing a 1 vs a 2
-        step = step[0] if step[2] > random() else step[1]    # (1, 2, 0.5) is full form          ## expand this to accommodate any number of options
+        step = step[0] if step[2] > random() else step[1]    # (1, 2, 0.5) is full form          ## expand this to accommodate any number of options?
         return step
 
     def _unroll(self, pattern, divs=None, r=None):    
