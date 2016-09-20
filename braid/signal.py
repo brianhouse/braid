bb51dca2d8e6d47473e18ae09352b6c9423d2def
brianhouse <brian.house@gmail.com>
2016-09-20 19:19:51 -0400

    adding tkinter plot functionality and signal generator for dealing with data

--- braid/signal.py
+++ braid/signal.py
@@ -37,3 +37,49 @@ def ease_out_in(pos):
         return 0.5 * pos**3 + 0.5
     else:
         return 1.0 - (0.5 * pos**3 + 0.5)
+
+def make_signal(timeseries):
+    timeseries = normalize(timeseries)
+    def signal_f(pos):
+        indexf = pos * (len(timeseries) - 1)
+        pos = indexf % 1.0
+        value = (timeseries[math.floor(indexf)] * (1.0 - pos)) + (timeseries[math.ceil(indexf)] * pos)
+        return value
+    return signal_f    
+
+def normalize(signal):
+    min_value = min(signal)
+    max_value = max(signal)
+    return [(v - min_value) / (max_value - min_value) for v in signal]    
+
+
+
+class Plotter():
+
+    instance = None
+
+    def __init__(self):
+        import tkinter
+        self.master = tkinter.Tk()
+        self.width, self.height = 1000, 250
+        self.margin = 20
+        self.w = tkinter.Canvas(self.master, width=self.width + self.margin*2, height=self.height + self.margin*2)
+        self.w.pack()
+        self.w.create_rectangle(self.margin - 1, self.margin - 1, self.width + self.margin + 1, self.height + self.margin + 1)
+
+    @classmethod
+    def plot(cls, bp_f, color="red"):
+        if cls.instance is None:
+            cls.instance = Plotter()
+        points = [(i + cls.instance.margin, ((1.0 - bp_f(float(i) / cls.instance.width)) * cls.instance.height) + cls.instance.margin) for i in range(int(cls.instance.width))]
+        cls.instance.w.create_line(points, fill=color, width=2.0)
+
+    @classmethod
+    def show_plots(cls):
+        cls.instance.master.update()
+
+def plot(signal, color="red"):
+    Plotter.plot(signal, color)
+
+def show_plots():
+    Plotter.show_plots()
