3d3df87d453dd362c5980acb49ad7edf1e412da4
brianhouse <brian.house@gmail.com>
2017-03-13 10:27:15 -0600

    documentation

--- examples/01_basic.py
+++ examples/01_basic.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-from braid import *         # import braid
+from braid import *        # import braid
 
-log_midi(True)              # choose whether to see MIDI output (False by default)
+log_midi(True)             # choose whether to see MIDI output (False by default)
 
-v1 = Thread(10)             # create a synth --  the number indicates the midi channel
-v1.pattern = C, C, C, C     # add a pattern
-v1.start()                  # start the synth
+t = Thread(1)              # create a thread -- the argument indicates the MIDI channel
+t.pattern = C, C, C, C     # add a pattern
+t.start()                  # start the thread
 
-start()                     # start the process
+start()                    # start the process
 
 
 
@@ -19,6 +19,13 @@ Make sure this file is executable:
 $ chmod +x ./examples/1_basic.py
 
 Run it:
-./examples/1_basic.py
+./examples/01_basic.py 0    # second parameter is index of MIDI device
+
+"""
+
+"""
+On terminology:
+
+This framework is called Braid, and the fundamental objects are called Threads -- a Thread corresponds to a hardware or software monosynth. It is not a thread in the programming sense -- Braid is single-threaded.
 
 """
--- examples/02_notes.py
+++ examples/02_notes.py
@@ -2,22 +2,38 @@
 
 from braid import *
 
-v1 = Thread(10)
+t = Thread(1)
 
 
 # MIDI pitch value can be specified by MIDI number or with note-name aliases between C0 and B8
 # C is an alias for C4, likewise for the rest of the middle octave
 
-v1.pattern = C, C, C, C
+t.pattern = C, C, C, C
 # is the same as
-v1.pattern = 60, 60, 60, 60
-
-
+t.pattern = 60, 60, 60, 60
 
 # rests are specified with a Z
-v1.pattern = C, Z, Z, C
+t.pattern = C, Z, Z, C
+
+# by default, there is no specified chord. But if there is one, notes can be specified by scale degree
+t.chord = C4, MAJ
+t.pattern = 1, 3, 5, 7
+
+# negative numbers designate the octave below
+t.pattern = -5, -7, 1, 5
+
+# a chord consists of a root note and a scale
+# built-in scales are: 
+# Ionian (ION), Dorian (DOR), Phrygian (PRG), Myxolydian (MYX), Aolian (AOL), Locrian (LOC), harmonic minor (MIN), ?? (MMI), blues (BLU), pentatonic (PEN), chromatic (CHR), gamelan slendra (SDR), gamelan ?? (PLG), jam (JAM)
+
+# custom scales can be generated with the following syntax, where numbers are chromatic steps from the root
+whole_tone_scale = Scale([0, 2, 4, 6, 8, 10])
+
+# R specifies a random note in the scale
+t.chord = C4, whole_tone_scale
+t.pattern = 1, R, R, -6
 
 
-v1.start()
+t.start()
 
 start()
