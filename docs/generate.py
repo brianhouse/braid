fc157c0ac8dc08ee323a45f5fd2efcd6ce6935b4
Brian House <email@brian.house>
2020-05-18 18:48:38 -0700

    fixed documentation highlighting

--- docs/generate.py
+++ docs/generate.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import os, markdown
+import os, markdown2
 
 with open(os.path.join(os.path.dirname(__file__), "page.html")) as f:
     page = f.read().strip()
 with open(os.path.join(os.path.dirname(__file__), "content.md")) as f:
-    content = markdown.markdown(f.read().strip(), extensions=["codehilite"]).replace("&gt;&gt;&gt;", "<span class=\"int\">&gt;&gt;&gt;</span>").replace("{{","<span class=\"output\">").replace("}}","</span>").replace("...", "<span class=\"int\">...</span>")
+    content = markdown2.markdown(f.read().strip(), extras=['fenced-code-blocks', 'codehilite']).replace("&gt;&gt;&gt;", "<span class=\"int\">&gt;&gt;&gt;</span>").replace("{{","<span class=\"output\">").replace("}}","</span>").replace("...", "<span class=\"int\">...</span>")
 with open(os.path.join(os.path.dirname(__file__), "index.html"), 'w') as f:
     f.write(page.replace("CONTENT", content))
